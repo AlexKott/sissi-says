5b79c207e8e3a7f11fbc8343b12bddc934f62031
Alina Beck <alina.beck@mail.com>
2017-12-12 12:19:09 +0700

    setup FormFieldBuilder with standard components

--- src/components/form/FormFieldBuilder.js
+++ src/components/form/FormFieldBuilder.js
@@ -1,15 +1,49 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Field } from 'redux-form';
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = (state, ownProps) => {
+  let component;
+  let type;
+  let options;
 
-});
+  switch(ownProps.fieldStructure.type) {
+    case 'string':
+      component = 'input';
+      type = 'text';
+      break;
+
+    case 'text':
+      component = 'textarea';
+      break;
+
+    case 'choice':
+      component = 'select';
+      options = ownProps.fieldStructure.choices;
+      break;
+      
+    default:
+      component = 'input';
+      type = 'text';
+  }
+
+  return {
+    fieldProps: { component, type, options },
+  };
+};
 
 const FormFieldBuilder = ({
   fieldName,
   fieldStructure,
+  fieldProps,
 }) => (
-  <div>hey ho</div>
+  <label>
+    {fieldStructure.label}:
+    <Field
+      name={fieldName}
+      {...fieldProps}
+    />
+  </label>
 );
 
 export default connect(mapStateToProps)(FormFieldBuilder);
