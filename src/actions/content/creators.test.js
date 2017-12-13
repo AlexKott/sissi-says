4a0f0d184e090a2b295ae89485fc7facb5169aa8
Alina Beck <alina.beck@mail.com>
2017-12-13 14:49:03 +0700

    restructure action tests

--- src/actions/content/creators.test.js
+++ src/actions/content/creators.test.js
@@ -2,30 +2,30 @@ import * as t from './types';
 import * as actions from './creators';
 
 describe('actions/content', () => {
-    describe('setInitialContent', () => {
-        it('should dispatch an action with the correct type and payload', () => {
-            const expectedAction = { type: t.SET_INITIAL_CONTENT };
-            const action = actions.setInitialContent();
+  describe('setInitialContent', () => {
+    it('should dispatch an action with the correct type and payload', () => {
+      const action = actions.setInitialContent();
 
-            expect(action).toEqual(expectedAction);
-        });
+      expect(action).toHaveProperty('type', t.SET_INITIAL_CONTENT);
     });
+  });
 
-    describe('addPage', () => {
-        it('should dispatch an action with the correct type and payload', () => {
-            const expectedAction = { type: t.ADD_PAGE, payload: { type: 'testType' }};
-            const action = actions.addPage('testType');
+  describe('addPage', () => {
+    it('should dispatch an action with the correct type and payload', () => {
+      const action = actions.addPage('testType');
 
-            expect(action).toEqual(expectedAction);
-        });
+      expect(action).toHaveProperty('type', t.ADD_PAGE);
+      expect(action.payload).toHaveProperty('type', 'testType');
     });
+  });
 
-    describe('addSection', () => {
-        it('should dispatch an action with the correct type and payload', () => {
-            const expectedAction = { type: t.ADD_SECTION, payload: { pageId: 'testPage', type: 'testType' }};
-            const action = actions.addSection('testPage', 'testType');
+  describe('addSection', () => {
+    it('should dispatch an action with the correct type and payload', () => {
+      const action = actions.addSection('testPage', 'testType');
 
-            expect(action).toEqual(expectedAction);
-        });
+      expect(action).toHaveProperty('type', t.ADD_SECTION);
+      expect(action.payload).toHaveProperty('pageId', 'testPage');
+      expect(action.payload).toHaveProperty('type', 'testType');
     });
+  });
 });
