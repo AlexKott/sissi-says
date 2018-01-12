7e1623d86e420523b77de14fbfd959d13659374f
Alex Kott <tetlokax@gmail.com>
2018-01-12 20:00:51 +0800

    save content files

--- api/fileController.js
+++ api/fileController.js
@@ -3,12 +3,26 @@ import path from 'path';
 import { promisify } from 'util';
 
 const readFileAsync = promisify(fs.readFile);
+const writeFileAsync = promisify(fs.writeFile);
 
 export function readJson(fileName) {
   return async ctx => {
     try {
       ctx.response.body = await readFileAsync(path.join(process.cwd(), `${fileName}.json`));
     } catch(error) {
+      ctx.response.body = {};
+    }
+  }
+}
+
+export function writeJson(fileName) {
+  return async ctx => {
+    const jsonData = ctx.request.body;
+    try {
+      await writeFileAsync(path.join(process.cwd(), `${fileName}.json`), JSON.stringify(jsonData));
+      ctx.response.status = 200;
+    } catch(error) {
+      ctx.response.status = 500;
       ctx.response.body = {};
     }
   }
--- api/router.js
+++ api/router.js
@@ -1,8 +1,10 @@
 import Router from 'koa-router';
-import { readJson } from './fileController';
+import { readJson, writeJson } from './fileController';
 const router = new Router();
 
 router.get('/structure', readJson('structure'));
 router.get('/content', readJson('content'));
+
+router.post('/content', writeJson('content'));
 
 export default router;
