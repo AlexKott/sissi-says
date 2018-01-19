2225b9e57ec8e64cb4b79d642bf583d440491e57
Alex Kott <tetlokax@gmail.com>
2018-01-19 14:59:53 +0800

    switch koa for express

--- api/index.js
+++ api/index.js
@@ -1,11 +1,17 @@
 import 'babel-polyfill';
-import Koa from 'koa';
-import koaBody from 'koa-body';
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 import router from './router';
 
-const app = new Koa();
+const app = express();
 const PORT = 3010;
 
-app.use(koaBody());
-app.use(router.routes());
+app.use(bodyParser.json());
+app.use(cors({
+  origin: 'http://localhost:3000',
+}));
+
+app.use('/', router);
+
 app.listen(PORT, () => console.log(`API listening on port ${PORT}`));
