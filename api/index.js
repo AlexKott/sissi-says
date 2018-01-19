52608e0b06175108f143c1bb9f0eb334f1b2010d
Alina Beck <alina.beck@mail.com>
2018-01-19 14:55:28 +0800

    Revert "add CORS to API"
    This reverts commit 8afac11b376b7ff5482b3f282163960a48fba943.

--- api/index.js
+++ api/index.js
@@ -1,15 +1,11 @@
 import 'babel-polyfill';
 import Koa from 'koa';
-import cors from 'koa-cors';
 import koaBody from 'koa-body';
 import router from './router';
 
 const app = new Koa();
 const PORT = 3010;
 
-app.use(cors({
-  origin: 'http://localhost:3000',
-}));
 app.use(koaBody());
 app.use(router.routes());
 app.listen(PORT, () => console.log(`API listening on port ${PORT}`));
