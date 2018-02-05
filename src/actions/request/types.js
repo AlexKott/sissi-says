45df4454543450799f14e7f42ec52797b18cab54
Alina Beck <alina.beck@mail.com>
2018-02-05 17:09:51 +0800

    remove unused action type

--- src/actions/request/types.js
+++ src/actions/request/types.js
@@ -1,3 +1,2 @@
 export const FETCH_DATA_SUCCESS = 'request/FETCH_DATA_SUCCESS';
-export const POST_CONTENT_SUCCESS = 'request/POST_CONTENT_SUCCESS';
 export const SEND_REQUEST = 'request/SEND_REQUEST';
