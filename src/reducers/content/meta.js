ceca7089db61edeebabae440f6af69362ec25092
Alina Beck <alina.beck@mail.com>
2018-02-08 19:00:53 +0800

    set empty object as initial value for meta reducer

--- src/reducers/content/meta.js
+++ src/reducers/content/meta.js
@@ -1,6 +1,6 @@
 import * as t from '@/actions/types';
 
-export default (state = [], action = {}) => {
+export default (state = {}, action = {}) => {
   const { type, payload } = action;
 
   if (type === t.FETCH_DATA_SUCCESS && payload.dataType === 'content') {
