aff4d07cdd9f6584eb5db4bf3e02d042f741b540
Alina Beck <alina.beck@mail.com>
2018-02-13 16:46:31 +0800

    display login form

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,12 +1,33 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+
+import Form from '@/components/form/Form';
+
+const loginFields = [
+  {
+    userName: {
+      label: 'Nutzername',
+      type: 'string',
+    },
+  },
+  {
+    password: {
+      label: 'Passwort',
+      type: 'password',
+    },
+  },
+];
 
 const Login = () => (
   <div className='login'>
+    <h1>Welcome to your Website Manager!</h1>
+    <p>Please login to edit your website.</p>
+    <Form
+      form='login'
+      fields={loginFields}
+      submitText='Login'
+      onSubmit={() => console.log('submitting')}
+    />
   </div>
 );
-
-Login.propTypes = {
-};
 
 export default Login;
