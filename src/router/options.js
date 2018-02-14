95b5a42996b35c74f9f8bfbead8783129e5eaa27
Alina Beck <alina.beck@mail.com>
2018-02-14 16:40:10 +0800

    redirect to login page when no token is found

--- src/router/options.js
+++ src/router/options.js
@@ -1,11 +1,18 @@
 import * as actions from '@/actions/creators';
 import * as selectors from '@/reducers/selectors';
+import * as routes from './routes';
 
 export default {
-  onBeforeChange(dispatch, getState) {
+  onBeforeChange(dispatch, getState, bag) {
+    const isLoggedIn = selectors.getAuthToken(getState()) !== null;
+    const location = getState().location.type;
+    const nextLocation = bag.action.type;
+    const isLoggingIn = location === routes.ROUTE_LOGIN || nextLocation === routes.ROUTE_LOGIN;
     const isInitialDataFetched = selectors.getIsInitialDataFetched(getState());
 
-    if (!isInitialDataFetched) {
+    if (!isLoggedIn && !isLoggingIn) {
+      dispatch(actions.redirectToLogin());
+    } else if (isLoggedIn && !isInitialDataFetched) {
       dispatch(actions.fetchData('structure'));
     }
   }
