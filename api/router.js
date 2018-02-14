16e47217e928deeed63129f52a6eeb9c7a11b396
Alina Beck <alina.beck@mail.com>
2018-02-14 15:18:53 +0800

    set up preliminary server response

--- api/router.js
+++ api/router.js
@@ -10,4 +10,14 @@ router.route('/content')
   .get(readJson('content'))
   .post(writeJson('content'));
 
+router.route('/login')
+  .post((req, res) => {
+    const { username, password } = req.body;
+    if (username === 'testUser' && password === '1234') {
+      res.send({ token: 'abc123' });
+    } else {
+      res.sendStatus(401);
+    }
+  });
+
 export default router;
