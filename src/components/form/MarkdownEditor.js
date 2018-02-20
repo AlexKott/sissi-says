1c0bccc4106980109bbb292449f0a025124d61a1
Alina Beck <alina.beck@mail.com>
2018-02-20 12:01:55 +0800

    connect editor to redux-form

--- src/components/form/MarkdownEditor.js
+++ src/components/form/MarkdownEditor.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import InscrybMDE from 'inscrybmde';
 
-class MarkdownEditor expands React.Component {
+class MarkdownEditor extends React.Component {
+  componentDidMount() {
+    const mde = new InscrybMDE({
+      element: document.querySelector('.markdown-editor'),
+      spellChecker: false,
+      status: false,
+      toolbar: [
+        'heading',
+        'italic',
+        'unordered-list',
+        'link',
+        'image',
+      ],
+    });
+
+    mde.codemirror.on('change', () => this.props.input.onChange(mde.value()));
+  }
+
   render() {
-    const { input } = this.props;
+    const { input, meta } = this.props;
     return (
-      <textarea {...input} />
+      <div className='markdown-editor__wrapper'>
+        <textarea className='markdown-editor' {...input} />
+      </div>
     );
   }
 }
