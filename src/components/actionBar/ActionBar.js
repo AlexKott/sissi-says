4b24e782a39065c961368b087c6b06b9a0d2388a
Alina Beck <alina.beck@mail.com>
2018-02-25 19:48:19 +0800

    use NavLink instead of MainButton component

--- src/components/actionBar/ActionBar.js
+++ src/components/actionBar/ActionBar.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { NavLink } from 'redux-first-router-link';
 
 import { ROUTE_INDEX } from '@/router/routes';
 
 import LogoutButton from './LogoutButton';
-import MainButton from './MainButton';
 import PublishButton from './PublishButton';
 
 const ActionBar = ({ route }) => (
   <div className='action-bar'>
     <PublishButton />
     <LogoutButton />
-    {route !== ROUTE_INDEX && <MainButton />}
+    <NavLink
+      to={{ type: ROUTE_INDEX }}
+      className='button'
+      activeClassName='hidden'
+      exact={true}
+    >Index</NavLink>
   </div>
 );
 
