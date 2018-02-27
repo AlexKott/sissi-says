0fddf15c40a23742f54831c2c58ef397d25befb9
Alina Beck <alina.beck@mail.com>
2018-02-27 12:41:21 +0800

    fix gatherRequestData tests

--- src/middleware/gatherRequestData.js
+++ src/middleware/gatherRequestData.js
@@ -4,18 +4,21 @@ import { transformToHtml } from '@/helpers/markdownHtmlConverter';
 import * as t from '@/actions/types';
 import * as selectors from '@/reducers/selectors';
 
-export default ({ dispatch, getState }, getters = selectors, collector = getFormValues) => next => action => {
+export default (
+  { dispatch, getState },
+  methods = { ...selectors, getFormValues, transformToHtml }
+) => next => action => {
   const { type, payload } = action;
   const isPostRequest = type === t.SEND_REQUEST && payload.method === 'post';
 
   if (isPostRequest && payload.dataType === 'content') {
     const { formName } = payload;
     const state = getState();
-    const metaData = getters.getMetaData(state);
-    const pageData = getters.getAllPages(state);
-    const sectionData = getters.getAllSections(state);
-    const formInput = collector(formName)(state);
-    const fields = getters.getFields(state);
+    const metaData = methods.getMetaData(state);
+    const pageData = methods.getAllPages(state);
+    const sectionData = methods.getAllSections(state);
+    const formInput = methods.getFormValues(formName)(state);
+    const fields = methods.getFields(state);
 
     let meta = metaData;
     let pages = pageData;
@@ -39,7 +42,7 @@ export default ({ dispatch, getState }, getters = selectors, collector = getForm
       }
     }
 
-    const transformedData = transformToHtml({ meta, pages, sections }, fields);
+    const transformedData = methods.transformToHtml({ meta, pages, sections }, fields);
 
     action.payload.requestData = transformedData;
   }
