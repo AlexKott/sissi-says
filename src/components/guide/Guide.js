e9aeeba5668c4b3c33febeb31fcd26c21dba0e41
Alina Beck <alina.beck@mail.com>
2018-02-28 17:51:43 +0800

    set up Guide component to receive props (wip)

--- src/components/guide/Guide.js
+++ src/components/guide/Guide.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import Sissi from '@/components/svgs/Sissi';
 
-const Guide = () => (
-  <div className='guide__button'>
+const Guide = ({ isGuideOpen, onCloseGuide }) => ([
+  isGuideOpen && <div onClick={onCloseGuide}>
+    The guide.
+  </div>
+  ,
+  <div key='guide-button' className='guide__button'>
     <Sissi />
   </div>
-);
+]);
+
+Guide.propTypes = {
+  isGuideOpen: PropTypes.bool,
+  onCloseGuide: PropTypes.func,
+};
 
 export default Guide;
