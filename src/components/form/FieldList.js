845ae8cc2020df3f3122b4c86607baf96be5c0e1
Alex Kott <tetlokax@gmail.com>
2018-09-21 18:22:59 +0530

    add buttons to fieldList, remove flexlist

--- src/components/form/FieldList.js
+++ src/components/form/FieldList.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { Translate } from 'react-localize-redux';
 
 import * as actions from '@/actions';
 import * as selectors from '@/selectors';
+import * as tr from '@/translations';
 
 import FormFieldBuilder from './FormFieldBuilder';
 
@@ -12,25 +14,59 @@ const mapStateToProps = (state, { name }) => ({
 });
 
 const mapDispatchToProps = (dispatch, { fields, name }) => ({
-  onAddItem: () => {
+  onAdd: () => {
     dispatch(actions.addListItem(name));
     fields.push({});
   },
-  onDeleteItem: index => dispatch(actions.deleteListItem(name, index)),
+  onDelete: index => dispatch(actions.deleteListItem(name, index)),
 });
 
-const FieldList = ({ fieldNames, fields, name }) => (
+const FieldList = ({
+  canAdd,
+  canDelete,
+  fieldNames,
+  fields,
+  itemLabel,
+  name,
+  onAdd,
+  onDelete,
+}) => (
   <section className='form__list'>
     <label className='form__label'>{name}</label>
     {fields.map((f, index) => (
       <article key={index} className='list-item'>
-        {fieldNames.map(fieldName => (<FormFieldBuilder
-          key={fieldName}
-          fieldName={fieldName}
-          prefix={`${f}.`}
-        />))}
+        {fieldNames.map(fieldName => [
+          <FormFieldBuilder
+            key={fieldName}
+            fieldName={fieldName}
+            prefix={`${f}.`}
+          />
+          ,
+          canDelete && (
+            <button
+              key={`delete-${index}`}
+              type='button'
+              onClick={() => onDelete(index)}
+              className='button'
+            >
+              <Translate id={tr.DELETE} />
+            </button>
+          )
+          ,
+        ])}
       </article>
     ))}
+    {canAdd && (
+      <article className='list-item'>
+        <button
+          type='button'
+          onClick={onAdd}
+          className='button'
+        >
+          + {itemLabel}
+        </button>
+      </article>
+    )}
   </section>
 );
 
@@ -39,9 +75,10 @@ FieldList.propTypes = {
   canDelete: PropTypes.bool,
   fieldNames: PropTypes.array,
   fields: PropTypes.object,
+  itemLabel: PropTypes.string,
   name: PropTypes.string,
-  onAddItem: PropTypes.func,
-  onDeleteItem: PropTypes.func,
+  onAdd: PropTypes.func,
+  onDelete: PropTypes.func,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(FieldList);
