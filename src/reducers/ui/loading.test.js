af6584fb9c681ed2c2f256c24200f32036b10b51
Alina Beck <alina.beck@mail.com>
2018-09-21 11:58:12 +0530

    add tests for loading reducer

--- src/reducers/ui/loading.test.js
+++ src/reducers/ui/loading.test.js
@@ -1,18 +1,52 @@
-import reducer, * as selectors from './loading';
+import _cloneDeep from 'lodash.clonedeep';
+
 import * as t from '@/actions/types';
+import _testState from '@/reducers/_testState';
+
+import reducer, * as selectors from './loading';
 
 describe('reducers/ui/loading', () => {
-  it('should increment loading', () => {
-    const action = { type: t.SET_LOADING, payload: 1 };
-    const value = reducer(0, action);
+  let mockState;
 
-    expect(value).toBe(1);
+  beforeEach(() => {
+    mockState = _cloneDeep(_testState.ui.loading);
+  });
+
+  it('should increment loading', () => {
+    const action = { type: t.SET_LOADING, payload: { diff: 1 }};
+    const state = reducer(mockState, action);
+
+    expect(state).toBe(1);
   });
 
   it('should decrement loading', () => {
-    const action = { type: t.SET_LOADING, payload: -1 };
-    const value = reducer(4, action);
+    mockState = 4;
+    const action = { type: t.SET_LOADING, payload: { diff: -1 }};
+    const state = reducer(mockState, action);
 
-    expect(value).toBe(3);
+    expect(state).toBe(3);
+  });
+});
+
+describe('selectors/ui/loading', () => {
+  let mockState;
+
+  beforeEach(() => {
+    mockState = _cloneDeep(_testState);
+  });
+
+  describe('getIsLoading', () => {
+    it('should return true if the state is bigger than 0', () => {
+      mockState.ui.loading = 2;
+      const value = selectors.getIsLoading(mockState);
+
+      expect(value).toBe(true);
+    });
+
+    it('should return false if the state is 0', () => {
+      const value = selectors.getIsLoading(mockState);
+
+      expect(value).toBe(false);
+    });
   });
 });
